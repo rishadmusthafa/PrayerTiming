7e31f4dffce09d517239497fa3cb3b2a3435a6fa
rishad <rishad.manatheril@icloud.com>
2024-12-15 07:35:13 +0530

    Initial Commit

---
+++ PrayerTiming/PrayerTimingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimingApp.swift
+//  PrayerTiming
+//
+//  Created by rishad on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrayerTimingTests/PrayerTimingTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerTimingTests.swift
+//  PrayerTimingTests
+//
+//  Created by rishad on 15/12/24.
+//
+
+import Testing
+@testable import PrayerTiming
+
+struct PrayerTimingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
